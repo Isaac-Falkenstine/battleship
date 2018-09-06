bf60ba48ce2a992543420ad1ea7a8e2fae2f8977
Isaac Falkenstine <isaac@falkenstine.com>
2018-09-05 20:00:57 -0600

    Worked on adding more functionality for board (WIP)

--- lib/board.rb
+++ lib/board.rb
@@ -30,4 +30,53 @@ class Board
       }
     }
   end
+
+
+  def anchor_ship(coordinates)
+      ship = Ship.new(coordinates)
+      coordinates.each {|spot|
+        key = spot.to_sym
+        positions[key][:player_map][:ship] = ship
+      }
+  end
+
+  def update_player_map(coord)
+    position = @positions[coord.to_sym][:player_map]
+    position[:shot] = true
+    if position[:ship] != nil
+      position[:hit] = true
+      position[:ship].update_status(coord)
+    end
+  end
+
+  def update_enemy_map(coord, success)
+    position = @positions[coord.to_sym][:enemy_map]
+    position[:shot] = true
+    position[:hit] = true if success
+  end
+  # TO DO --- ADD successful_hit method
+    # player or board???
+
+  def possible_positions(width)
+    arr = prevent_overlap
+    sets = collect_sets(arr, width)
+    sets.reject!{|set| set.include?(nil)}
+    return sets
+  end
+
+  def collect_sets(arr, width)
+    sets1 = loop_through_rows_for_sets(arr, width)
+    sets2 = loop_through_rows_for_sets(arr.transpose, width)
+    sets = sets1 + sets2
+    sets = sets.flatten!.each_slice(width).to_a
+  end
+
+  def prevent_overlap
+    arr = create_positions
+    arr.each.with_index { |pos, i|
+      key = pos.to_sym
+      arr[i] = nil if @positions[key][:player_map][:ship]
+    }
+    return arr.each_slice(@size).to_a
+  end
 end
--- test/board_test.rb
+++ test/board_test.rb
@@ -1,6 +1,7 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/board'
+require './lib/ship'
 
 
 class BoardTest < Minitest::Test
@@ -43,38 +44,101 @@ class BoardTest < Minitest::Test
   end
 
   def test_it_creates_position_labels
-  default = Board.new
-  expected = [
-    "A1", "A2", "A3", "A4",
-    "B1", "B2", "B3", "B4",
-    "C1", "C2", "C3", "C4",
-    "D1", "D2", "D3", "D4"
-  ]
-  assert_equal expected, default.create_positions
+    default = Board.new
+    expected = [
+      "A1", "A2", "A3", "A4",
+      "B1", "B2", "B3", "B4",
+      "C1", "C2", "C3", "C4",
+      "D1", "D2", "D3", "D4"
+    ]
+    assert_equal expected, default.create_positions
 
-  assigned = Board.new(2)
-  expected = [
-    "A1", "A2",
-    "B1", "B2"
-  ]
-  assert_equal expected, assigned.create_positions
-end
+    assigned = Board.new(2)
+    expected = [
+      "A1", "A2",
+      "B1", "B2"
+    ]
+    assert_equal expected, assigned.create_positions
+  end
 
-def test_it_can_initialize_positions_with_default_states
-  board = Board.new(2)
-  positions = board.positions
-  assert_equal empty = {}, positions
-  board.initialize_positions
+  def test_it_can_initialize_positions_with_default_states
+    board = Board.new(2)
+    positions = board.positions
+    assert_equal empty = {}, positions
+    board.initialize_positions
 
-  assert_equal false, positions[:A1][:player_map][:shot]
-  assert_equal false, positions[:A1][:player_map][:hit]
-  assert_equal nil,   positions[:A1][:player_map][:ship]
-  assert_equal false, positions[:A1][:enemy_map][:shot]
-  assert_equal false, positions[:A1][:enemy_map][:hit]
+    assert_equal false, positions[:A1][:player_map][:shot]
+    assert_equal false, positions[:A1][:player_map][:hit]
+    assert_equal nil,   positions[:A1][:player_map][:ship]
+    assert_equal false, positions[:A1][:enemy_map][:shot]
+    assert_equal false, positions[:A1][:enemy_map][:hit]
 
-  assert_equal false, positions[:A2][:player_map][:shot]
-  assert_equal nil,   positions[:B1][:player_map][:ship]
-  assert_equal false, positions[:B2][:enemy_map][:hit]
-end
+    assert_equal false, positions[:A2][:player_map][:shot]
+    assert_equal nil,   positions[:B1][:player_map][:ship]
+    assert_equal false, positions[:B2][:enemy_map][:hit]
+  end
+  def test_it_can_anchor_ship
+      board = Board.new(2)
+      board.initialize_positions
+      position1 = board.positions[:A1]
+      position2 = board.positions[:A2]
+      assert_nil position1[:player_map][:ship]
+      assert_nil position2[:player_map][:ship]
+      board.anchor_ship(["A1", "A2"])
+      assert_instance_of Ship, position1[:player_map][:ship]
+      assert_instance_of Ship, position2[:player_map][:ship]
+      assert_equal ["A1", "A2"], position1[:player_map][:ship].coordinates
+  end
 
+  def test_it_can_update_player_map_with_enemy_shots
+    board = Board.new(2)
+    positions = board.positions
+    board.initialize_positions
+    assert_equal false, positions[:A1][:player_map][:shot]
+    board.anchor_ship(["A1"])
+    board.update_player_map("A1")
+    assert_equal true, positions[:A1][:player_map][:shot]
+    assert_equal true, positions[:A1][:player_map][:hit]
+  end
+
+  def test_it_can_update_actualizations_of_enemy_map
+    board = Board.new(2)
+    positions = board.positions
+    board.initialize_positions
+    assert_equal false, positions[:A1][:enemy_map][:shot]
+    board.update_enemy_map("A1", false)
+    assert_equal true, positions[:A1][:enemy_map][:shot]
+    assert_equal false, positions[:A1][:enemy_map][:hit]
+    board.update_enemy_map("A1", true)
+    assert_equal true, positions[:A1][:enemy_map][:shot]
+    assert_equal true, positions[:A1][:enemy_map][:hit]
+  end
+
+  def test_it_can_create_possible_positions_for_a_ship
+    board = Board.new
+    board.initialize_positions
+
+    choices_for_2 = board.possible_positions(2)
+    assert_equal 2, choices_for_2[0].count
+    assert_equal 3*4*2, choices_for_2.count
+
+    choices_for_3 = board.possible_positions(3)
+    assert_equal 3, choices_for_3[0].count
+    assert_equal 2*4*2, choices_for_3.count
+    board.anchor_ship(["A1", "A2"])
+    choices_for_3 = board.possible_positions(3)
+    assert_equal (2*4*2)-4, choices_for_3.count
+  end
+
+  def test_it_can_prevent_ship_overlap
+    board = Board.new
+    board.initialize_positions
+    no_nil = board.prevent_overlap
+    actual = no_nil.any? { |set| set.include?(nil) }
+    assert_equal false, actual
+    board.anchor_ship(["A1", "A2"])
+    with_nil = board.prevent_overlap
+    actual = with_nil.any? { |set| set.include?(nil) }
+    assert_equal true, actual
+  end
 end
